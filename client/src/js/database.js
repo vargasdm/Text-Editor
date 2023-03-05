141a65c820789197e473de3f3fd1d57b4352403a
vargasdm <112888008+vargasdm@users.noreply.github.com>
2023-03-04 22:21:30 -0500

    fixed get and put methods

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,14 +13,14 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
-  // console.log('PUT to the database');
-  // const todosDb = await openDB('jate', 1);
-  // const tx = todosDb.transaction('jate', 'readwrite');
-  // const store = tx.objectStore('jate');
-  // const request = store.put({ id: id, value: content });
-  // const result = await request;
-  // console.log('🚀 - data saved to the database', result);
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const todosDb = await openDB('jate', 1);
+  const tx = todosDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -37,8 +37,7 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
-
+  const request = store.get(1);
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
