1a6009d7b2849d14a87334c213a7f22f73523a75
vargasdm <112888008+vargasdm@users.noreply.github.com>
2023-03-05 10:13:20 -0500

    fixed codemirror error

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -34,7 +34,7 @@ export const getDb = async () => {
   const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 initdb();
